5f05d0d51c2effd2de653ff14d8dce074163d391
hfzfdlh <fadillahhafiz.fadillah@gmail.com>
2023-09-26 13:07:49 +0700

    feat:filter by area

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const { getUserById, editUser, deleteUser, generateMidtransToken, midtransWebhook } = require('../controllers/user')
+const { uploadProfileUser } = require('../helpers/imageUploader')
 const userRouter = express.Router()
 
 userRouter.get('/profile/:id', getUserById)
-userRouter.put('/profile/:id', editUser)
+userRouter.put('/profile/:id', uploadProfileUser, editUser)
 userRouter.delete('/profile/:id', deleteUser)
 userRouter.post('/generate-midtrans-token', generateMidtransToken)
-userRouter.post('midtrans-webhook', midtransWebhook)
+userRouter.post('/midtrans-webhook', midtransWebhook)
 
 module.exports = userRouter
